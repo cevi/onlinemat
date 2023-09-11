71da3f4ffc0720ef4319553bcd24eb2b91e0e7c0
pemko123 <kutil@gmx.ch>
2023-09-11 19:31:24 +0200

    added standort table to view all standorte

--- src/views/abteilung/standort/abteilungStandorte.tsx
+++ src/views/abteilung/standort/abteilungStandorte.tsx
@@ -1,24 +1,12 @@
-import { useContext, useState } from 'react';
-import classNames from 'classnames';
-import appStyles from 'styles.module.scss';
-import { Spin, Input, Radio, message, Row, Col } from 'antd';
-import { AddMaterialButton } from 'components/material/AddMaterial';
-import { AppstoreOutlined, MenuOutlined } from '@ant-design/icons';
-import { AddCategorieButton } from 'components/categorie/AddCategorie';
+import { useContext } from 'react';
+import { Spin, Row, Col } from 'antd';
 import { Abteilung } from 'types/abteilung.type';
-import { MaterialTable } from 'components/material/MaterialTable';
-import { MaterialGrid } from 'components/material/MaterialGrid';
 import { Can } from 'config/casl/casl';
 import { AbteilungEntityCasl } from 'config/casl/ability';
-import {CategorysContext, MaterialsContext, StandorteContext} from 'components/abteilung/AbteilungDetails';
-import { useCookies } from 'react-cookie';
-import { CartItem } from 'types/cart.types';
-import { cookieToCart, getCartName } from 'util/CartUtil';
-import moment from 'moment';
-import { Material } from 'types/material.types';
-import { getAvailableMatCount } from 'util/MaterialUtil';
+import {StandorteContext} from 'components/abteilung/AbteilungDetails';
 import {AddStandortButton} from "components/standort/AddStandort";
 import {StandortTable} from "../../../components/standort/StandortTable";
+import {Standort} from "../../../types/standort.types";
 
 export type AbteilungStandorteViewProps = {
     abteilung: Abteilung;
@@ -26,15 +14,6 @@ export type AbteilungStandorteViewProps = {
 
 export const AbteilungStandorteView = (props: AbteilungStandorteViewProps) => {
     const { abteilung } = props;
-
-    const { Search } = Input;
-
-    const cookieName = getCartName(abteilung.id);
-
-    const [cookies, setCookie] = useCookies([cookieName]);
-
-
-    const [query, setQuery] = useState<string | undefined>(undefined);
 
     //fetch standort
     const standorteContext = useContext(StandorteContext);
@@ -51,16 +30,6 @@ export const AbteilungStandorteView = (props: AbteilungStandorteViewProps) => {
             <Can I={'create'} this={{ __caslSubjectType__: 'Standort', abteilungId: abteilung.id } as AbteilungEntityCasl}>
                 <AddStandortButton abteilungId={abteilung.id} />
             </Can>
-        </Col>
-
-        <Col span={20}>
-            <Search
-                placeholder='nach Standort suchen'
-                allowClear
-                enterButton='Suchen'
-                size='large'
-                onSearch={(query) => setQuery(query)}
-            />
         </Col>
 
         <Col span={24}>
