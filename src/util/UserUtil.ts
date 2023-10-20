0e79f796a53baf00d8d372f07e44f5f217089177
Jan Preisig <jan.preisig@intuitive-collaboration.com>
2023-10-20 11:44:21 +0200

    #42 Create UserUtil Klasse

---
+++ src/util/UserUtil.ts
@@ -0,0 +1,19 @@
+import {firestore} from "../config/firebase/firebase";
+import {usersCollection} from "../config/firebase/collections";
+import {message} from "antd";
+import {AppUser} from "../config/redux/user/user";
+import {UserData} from "../types/user.type";
+
+export const dateFormat = 'DD.MM.YYYY';
+export const dateFormatWithTime = 'DD.MM.YYYY HH:mm';
+
+export const editUserData = async (userId: string | undefined, userData: UserData) => {
+    try {
+
+        await firestore().collection(usersCollection).doc(userId).update(userData);
+        message.success(`User ${userData.name} erfolgreich bearbeitet`);
+    } catch (ex) {
+        message.error(`Es ist ein Fehler aufgetreten: ${ex}`)
+    }
+
+}
